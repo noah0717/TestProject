88e454dc15e57870de3207eeca31f0623df4c7d4
invierno902 <invierno902@gmail.com>
2023-12-15 17:36:26 +0900

    hihi hihi1 	modified:   src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <a href="/returnStr_cubic">JH_cubic api REtest</a><br>
+  <a href="/returnStr_cubic">111JH_cubic api REtest</a><br>
   <a href="/example">예제 api</a>
 </template>
 
